3cd92a71796fa398e82b84652b48681af55abdb5
RishikeshKeshav <rishikeshandhare1995@gmail.com>
2024-02-06 10:48:02 -0800

    variables

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 145 //cannot change
+let email="rishikesh@gmail.com" 
+var accountName = "rishikesh"
+city = "akola"
+let accountState ;
+
+
+console.table([accountId,accountName,email,city,accountState])
