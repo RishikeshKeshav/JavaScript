a28e2e2b8d45795689df74a61d250de016e56a0a
RishikeshKeshav <rishikeshandhare1995@gmail.com>
2024-02-06 18:25:16 -0800

    objects2

---
+++ Object_And_Array/Objects2.js
@@ -0,0 +1,67 @@
+//Single ton
+
+
+const user = new Object()
+
+const user1 = {}
+
+user1.id = "0001"
+user1.name= "rishikesh"
+user1.age = 32
+console.log(user)
+console.log(user1)
+// console.log(`${user},${user1}`)
+
+
+const regularUser = {
+    id:"0939",
+    fullename:{
+        firstname:"rishi",
+        lastname:"andhare"
+    },
+    dateofbirth:{
+        city:"akola",
+        state:"MH"
+    }
+}
+
+console.log(regularUser)
+console.log(regularUser.fullename.firstname)
+
+const obj1 ={
+    1:"a",
+    2:"bc",
+    3:"asd",
+    4:"akdjf"
+}
+const obj2 ={
+    5:"asd",
+    6:"bcd",
+    7:"asdd",
+    8:"akdjfd"
+}
+ const obj3 = Object.assign({},obj1,obj2)
+console.log(obj3)
+const val =9
+const obj4 = {...obj1,...obj2,...obj3}
+console.log(obj4)
+console.log(`object 4 + ${val}`)
+
+const users = [
+    {
+        id:1,
+        name:"rishi"
+
+},
+{
+    id:3,
+    name:"isi"
+}]
+
+console.log(users[1].id)
+
+
+
+console.log(Object.keys(user1))
+console.log(Object.values(user1))
+console.log(Object.entries(user1))
